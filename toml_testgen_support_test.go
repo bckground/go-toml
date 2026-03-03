955e4b4fcdc44824180a0f12425e23930765183f
João Fernandes <joao@bckground.com>
2026-03-03 16:04:21 +0000

    refactor: use test tables

--- toml_testgen_support_test.go
+++ toml_testgen_support_test.go
@@ -1,11 +1,9 @@
-//go:generate go run github.com/toml-lang/toml-test/v2/cmd/toml-test@v2.1.0 copy -toml 1.1 ./tests
-//go:generate go run ./cmd/tomltestgen/main.go -r v2.1.0 -o toml_testgen_test.go
-
 package toml_test
 
 import (
 	"encoding/json"
 	"errors"
+	"sort"
 	"testing"
 
 	"github.com/pelletier/go-toml/v2"
@@ -13,48 +11,67 @@ import (
 	"github.com/pelletier/go-toml/v2/internal/testsuite"
 )
 
-func testgenInvalid(t *testing.T, input string) {
-	t.Helper()
-	t.Logf("Input TOML:\n%s", input)
+func TestTOMLTest_Invalid(t *testing.T) {
+	names := make([]string, 0, len(testgenInvalidCases))
+	for name := range testgenInvalidCases {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		t.Run(name, func(t *testing.T) {
+			input := testgenInvalidCases[name]
+			t.Logf("Input TOML:\n%s", input)
 
-	doc := map[string]interface{}{}
-	err := testsuite.Unmarshal([]byte(input), &doc)
+			doc := map[string]interface{}{}
+			err := testsuite.Unmarshal([]byte(input), &doc)
 
-	if err == nil {
-		out, err := json.Marshal(doc)
-		if err != nil {
-			panic("could not marshal map to json")
-		}
-		t.Log("JSON output from unmarshal:", string(out))
-		t.Fatalf("test did not fail")
+			if err == nil {
+				out, err := json.Marshal(doc)
+				if err != nil {
+					panic("could not marshal map to json")
+				}
+				t.Log("JSON output from unmarshal:", string(out))
+				t.Fatalf("test did not fail")
+			}
+		})
 	}
 }
 
-func testgenValid(t *testing.T, input string, jsonRef string) {
-	t.Helper()
-	t.Logf("Input TOML:\n%s", input)
-
-	// TODO: change this to interface{}
-	var doc map[string]interface{}
-
-	err := testsuite.Unmarshal([]byte(input), &doc)
-	if err != nil {
-		de := &toml.DecodeError{}
-		if errors.As(err, &de) {
-			t.Logf("%s\n%s", err, de)
-		}
-		t.Fatalf("failed parsing toml: %s", err)
+func TestTOMLTest_Valid(t *testing.T) {
+	names := make([]string, 0, len(testgenValidCases))
+	for name := range testgenValidCases {
+		names = append(names, name)
 	}
-	j, err := testsuite.ValueToTaggedJSON(doc)
-	assert.NoError(t, err)
+	sort.Strings(names)
+	for _, name := range names {
+		t.Run(name, func(t *testing.T) {
+			input := testgenValidCases[name]
+			jsonRef := testgenValidJSONRef[name]
+			t.Logf("Input TOML:\n%s", input)
 
-	var ref interface{}
-	err = json.Unmarshal([]byte(jsonRef), &ref)
-	assert.NoError(t, err)
+			// TODO: change this to interface{}
+			var doc map[string]interface{}
 
-	var actual interface{}
-	err = json.Unmarshal(j, &actual)
-	assert.NoError(t, err)
+			err := testsuite.Unmarshal([]byte(input), &doc)
+			if err != nil {
+				de := &toml.DecodeError{}
+				if errors.As(err, &de) {
+					t.Logf("%s\n%s", err, de)
+				}
+				t.Fatalf("failed parsing toml: %s", err)
+			}
+			j, err := testsuite.ValueToTaggedJSON(doc)
+			assert.NoError(t, err)
 
-	testsuite.CmpJSON(t, "", ref, actual)
+			var ref interface{}
+			err = json.Unmarshal([]byte(jsonRef), &ref)
+			assert.NoError(t, err)
+
+			var actual interface{}
+			err = json.Unmarshal(j, &actual)
+			assert.NoError(t, err)
+
+			testsuite.CmpJSON(t, "", ref, actual)
+		})
+	}
 }
